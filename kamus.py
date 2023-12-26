4e952b5715d06d4964bae75a05302800956fc93b
pralanggaarkabuana <154896264+pralanggaarkabuana@users.noreply.github.com>
2023-12-26 20:42:03 +0700

    Create kamus.py
    First commit

---
+++ kamus.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "FR" : "Kepanjangan dari for real yang digunakan sebagai tanggapan jika meragukan tentang suatu hal",
+            }
+
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+
+if word in meme_dict.keys():
+    # Apa yang harus kita lakukan jika kata itu ditemukan?
+    print(meme_dict[word])
+else:
+    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+    print("Maaf kata belum tersedia")
